b7a78eca62c4d5da7ab607ffe6c664129c67a3a5
Spectre <elouan.fare@protonmail.ch>
2024-07-21 16:11:47 +0200

    start

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+//use rand::{random, thread_rng, Rng};
+#[derive(Clone, Copy)]
+struct Cell {
+    visited: bool,
+    walls: [bool; 4], // murs : [nord, sud, est, ouest]
+}
 fn main() ->() {
     println!("test du premier algo de Maze appeller \"Prim\"");
 }
-fn GenerateMaze(width:u32, height:u32)
+fn GenerateMaze(width:u32, height:u32) ->() {
+    //let grille = thread_rng().gen(i32);
+}
